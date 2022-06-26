be30a03ffc736965a3eafe688a46556b27adda75
Ubuntu <ubuntu@ip-172-31-89-229.ec2.internal>
2022-06-26 03:02:55 +0000

    feat: success of faucet

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from sqlalchemy.orm import Session
 from sqlalchemy.sql import text
 
 from logger import logger
-from ape import accounts
+from ape import accounts, networks
 
 load_dotenv()
 TOKEN = os.getenv("TOKEN")
@@ -76,11 +76,10 @@ async def faucet(message):
     payload = message.content
     wallet_address = payload.split(" ")[1]
     discord_id = message.author.id
-    date_time = time.time()
     db_obj = Faucet(
         wallet_address=wallet_address, 
         discord_id=discord_id, 
-        time=date_time
+        time=0
     )
     with Session(engine) as db:
         wallet_obj = db.query(
@@ -90,10 +89,8 @@ async def faucet(message):
             db.add(db_obj)
             db.commit()
             db.refresh(db_obj)
-    if not wallet_obj:
-        accounts.transfer(TEST_ACCOUNT, wallet_address, 100)
-    else:
-        send_to_wallet(db_obj.wallet_address, db_obj.discord_id)
+    msg = send_to_wallet(db_obj.wallet_address, int(db_obj.discord_id))
+    await(send_message(message,msg))
     return get_faucets()
 
 
@@ -102,14 +99,16 @@ def get_wallet_address(wallet_identifier):
         if isinstance(wallet_identifier, str):
             query_wallet = db.query(
                 Faucet).filter(
-                Faucet.wallet_address == wallet_address).first()
+                Faucet.wallet_address == wallet_identifier).first()
         elif isinstance(wallet_identifier, int):
             query_wallet = db.query(
                 Faucet).filter(
                 Faucet.discord_id == wallet_identifier).first()
-        check_available = check_available_to_send(query_wallet)
-    if check_available:
+    check_available = check_available_to_send(query_wallet)
+    if check_available and isinstance(wallet_identifier, str):
         return query_wallet.wallet_address
+    elif check_available and isinstance(wallet_identifier, int):
+        return query_wallet.discord_id
     return None
 
 
@@ -120,19 +119,31 @@ def check_available_to_send(query):
         return True
     return False
    
-async def send_to_wallet(wallet_address, discord_id):
-    wallet_address = get_wallet_address(wallet_address)
-    discord_id = get_wallet_address(discord_id)
-    if not wallet_address or not discord_id:
-        await message.channel.send("You need to wait 24 hours from your last request")
-        #todo: tell user how much time they have to wait more
-    elif not TEST_ACCOUNT.balance >= 100:
-        await message.channel.send("Not enough ETH in faucet")
-    elif wallet_address and discord_id:
-        accounts.transfer(TEST_ACCOUNT, wallet_address, 100)
-        update_db_obj(wallet_address, discord_id)
-    else:
-        await message.channel.send("contact an ape representative, there has been an issue")
+def send_to_wallet(_wallet_address, _discord_id):
+    wallet_address = get_wallet_address(_wallet_address)
+    discord_id = get_wallet_address(_discord_id)
+    with networks.get_ecosystem("ethereum").local.use_default_provider() as provider:
+        breakpoint()
+        TEST_ACCOUNT.transfer(wallet_address, TEST_ACCOUNT.balance - 10**15)
+        if not wallet_address or not discord_id:
+            message = "You need to wait 24 hours from your last request"
+            #todo: tell user how much time they have to wait more
+        elif not TEST_ACCOUNT.balance >= 100:
+            message = "Not enough ETH in faucet"
+        elif wallet_address and discord_id:
+            amount = 100
+            TEST_ACCOUNT.transfer(wallet_address, amount)
+            logger.info(f"{TEST_ACCOUNT.balance}: moved 100 wei to {wallet_address}")
+            update_db_obj(wallet_address, discord_id)
+            message = "eth sent"
+        else:
+            message = "contact an ape representative, there has been an issue"
+    return message
+
+@client.event
+async def send_message(message, msg):
+    await message.channel.send(msg)
+
 
 
 def update_db_obj(wallet_address, discord_id):
@@ -216,23 +227,23 @@ async def on_message(message):
     if message.author == client.user:
         return
         # echos a bug to the targeted channel
-        try:
-            if message.content.startswith("$bug"):
-                await echo(message, eth_guild, ape_guild)
-                logger.info(message)
-            # send testnet eth to user
-            elif message.content.startswith("$faucet"):
-                await faucet(message)
-            # says hello to the user
-            elif message.content.startswith("$hello"):
-                await message.channel.send("Hello")
-            elif message.guild.name == ETH_GUILD and not message.content.startswith(
-                "$bug"
-            ):
-                await echo(message, eth_guild, ape_guild)
-                logger.info(message)
-        except Exception as err:
-            logger.error(err)
+    try:
+        if message.content.startswith("$bug"):
+            await echo(message, eth_guild, ape_guild)
+            logger.info(message)
+        # send testnet eth to user
+        elif message.content.startswith("$faucet"):
+            await faucet(message)
+        # says hello to the user
+        elif message.content.startswith("$hello"):
+            await message.channel.send("Hello")
+        elif message.guild.name == ETH_GUILD and not message.content.startswith(
+            "$bug"
+        ):
+            await echo(message, eth_guild, ape_guild)
+            logger.info(message)
+    except Exception as err:
+        logger.error(err)
 
 
 def get_gas():
